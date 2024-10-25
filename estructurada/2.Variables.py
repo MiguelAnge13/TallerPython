a8d20f6dc7ea5bec04cf91a3e6d4f77dde694dff
Miguel Angel Marquez Mendez <miguel.angel@gmail.com>
2024-10-25 15:33:03 -0600

    Primeros programas en Python

---
+++ estructurada/2.Variables.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input("Dame un numero: "))
+    b = int(input("Dame otro numero: "))
+    c = int(input("Con este numero terminamos: "))
+
+    resultado = a*a+b*b+c*c
+    print(f"El sultado es {resultado}")
